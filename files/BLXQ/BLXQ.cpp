d6af329c147cc2206f7c699767e826a8c3eebc43
sqmidl18612 <q78257877141328@163.com>
2021-06-05 10:12:55 +0800

    BLXQ

---
+++ files/BLXQ/BLXQ.cpp
@@ -0,0 +1,299 @@
+// BLXQ.cpp : Defines the entry point for the application.
+//
+
+#include "stdafx.h"
+#include "resource.h"
+
+#define MAX_LOADSTRING 100
+
+// 窗口和绘图属性
+const int WINDOW_STYLES = WS_OVERLAPPED | WS_SYSMENU | WS_CAPTION | WS_MINIMIZEBOX;
+
+// 棋子编号
+const int PIECE_KING = 0;
+const int PIECE_PAWN = 6;
+
+// 棋盘初始设置
+static const BYTE cucpcStartup[256] = {
+  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+  0,  0,  0, 20, 19, 18, 17, 16, 17, 18, 19, 20,  0,  0,  0,  0,
+  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+  0,  0,  0,  0, 21,  0,  0,  0,  0,  0, 21,  0,  0,  0,  0,  0,
+  0,  0,  0, 22,  0, 22,  0, 22,  0, 22,  0, 22,  0,  0,  0,  0,
+  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+  0,  0,  0, 14,  0, 14,  0, 14,  0, 14,  0, 14,  0,  0,  0,  0,
+  0,  0,  0,  0, 13,  0,  0,  0,  0,  0, 13,  0,  0,  0,  0,  0,
+  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+  0,  0,  0, 12, 11, 10,  9,  8,  9, 10, 11, 12,  0,  0,  0,  0,
+  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0
+};
+
+
+// 获得红黑标记(红子是8，黑子是16)
+inline int SIDE_TAG(int sd) {
+  return 8 + (sd << 3);
+}
+
+
+// 局面结构
+struct PositionStruct {
+  int sdPlayer;                   // 轮到谁走，0=红方，1=黑方
+  BYTE ucpcSquares[256];          // 棋盘上的棋子
+
+  void Startup(void) {            // 初始化棋盘
+    sdPlayer = 0;
+    memcpy(ucpcSquares, cucpcStartup, 256);
+  }
+  void ChangeSide(void) {         // 交换走子方
+    sdPlayer = 1 - sdPlayer;
+  }
+  void AddPiece(int sq, int pc) { // 在棋盘上放一枚棋子
+    ucpcSquares[sq] = pc;
+  }
+  void DelPiece(int sq) {         // 从棋盘上拿走一枚棋子
+    ucpcSquares[sq] = 0;
+  }
+  void MovePiece(int mv);         // 搬一步棋的棋子
+  void MakeMove(int mv) {         // 走一步棋
+    MovePiece(mv);
+    ChangeSide();
+  }
+};
+
+static PositionStruct pos; // 局面实例
+
+// 与图形界面有关的全局变量
+static struct {
+  HINSTANCE hInst;                              // 应用程序句柄实例
+  HWND hWnd;                                    // 主窗口句柄
+  HDC hdc, hdcTmp;                              // 设备句柄，只在"ClickSquare"过程中有效
+  HBITMAP bmpBoard, bmpSelected, bmpPieces[24]; // 资源图片句柄
+  int sqSelected, mvLast;                       // 选中的格子，上一步棋
+  BOOL bFlipped;                                // 是否翻转棋盘
+} Xqwl;
+
+// 初始化棋局
+static void Startup(void) {
+  pos.Startup();
+  Xqwl.sqSelected = Xqwl.mvLast = 0;
+}
+
+
+// Global Variables:
+HINSTANCE hInst;								// current instance
+TCHAR szTitle[MAX_LOADSTRING];								// The title bar text
+TCHAR szWindowClass[MAX_LOADSTRING];								// The title bar text
+
+// Foward declarations of functions included in this code module:
+ATOM				MyRegisterClass(HINSTANCE hInstance);
+BOOL				InitInstance(HINSTANCE, int);
+LRESULT CALLBACK	WndProc(HWND, UINT, WPARAM, LPARAM);
+LRESULT CALLBACK	About(HWND, UINT, WPARAM, LPARAM);
+
+// 装入资源图片
+inline HBITMAP LoadResBmp(int nResId) {
+  return (HBITMAP) LoadImage(Xqwl.hInst, MAKEINTRESOURCE(nResId), 
+	                    IMAGE_BITMAP, 0, 0, 
+						LR_DEFAULTSIZE | LR_SHARED);
+}
+
+
+int APIENTRY WinMain(HINSTANCE hInstance,
+                     HINSTANCE hPrevInstance,
+                     LPSTR     lpCmdLine,
+                     int       nCmdShow)
+{
+	
+  int i;
+  MSG msg;
+  WNDCLASSEX wce;
+
+  // 初始化全局变量
+  Xqwl.hInst = hInstance;
+  Xqwl.bFlipped = FALSE;
+  Startup();
+
+  // 装入图片
+  Xqwl.bmpBoard = LoadResBmp(IDB_BOARD);
+  Xqwl.bmpSelected = LoadResBmp(IDB_SELECTED);
+  for (i = PIECE_KING; i <= PIECE_PAWN; i ++) {
+    Xqwl.bmpPieces[SIDE_TAG(0) + i] = LoadResBmp(IDB_RK + i);
+    Xqwl.bmpPieces[SIDE_TAG(1) + i] = LoadResBmp(IDB_BK + i);
+  }
+
+  // 设置窗口
+  wce.cbSize = sizeof(WNDCLASSEX);
+  wce.style = 0;
+  wce.lpfnWndProc = (WNDPROC) WndProc;
+  wce.cbClsExtra = wce.cbWndExtra = 0;
+  wce.hInstance = hInstance;
+  wce.hIcon = (HICON) LoadImage(hInstance, 
+	  MAKEINTRESOURCE(IDI_BLXQ), IMAGE_ICON, 32, 32, LR_SHARED);
+  wce.hCursor = (HCURSOR) LoadImage(NULL, IDC_ARROW, IMAGE_CURSOR, 0, 0, LR_DEFAULTSIZE | LR_SHARED);
+  wce.hbrBackground = (HBRUSH) (COLOR_BTNFACE + 1);
+  wce.lpszMenuName = MAKEINTRESOURCE(IDM_MAINMENU);
+  wce.lpszClassName = "XQWLIGHT";
+  wce.hIconSm = (HICON) LoadImage(hInstance, 
+	  MAKEINTRESOURCE(IDI_BLXQ), IMAGE_ICON, 16, 16, LR_SHARED);
+  RegisterClassEx(&wce);
+
+  // 打开窗口
+  Xqwl.hWnd = CreateWindow("XQWLIGHT", "象棋小巫师", WINDOW_STYLES,
+      CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, 
+	  NULL, NULL, hInstance, NULL);
+  if (Xqwl.hWnd == NULL) {
+    return 0;
+  }
+  ShowWindow(Xqwl.hWnd, nCmdShow);
+  UpdateWindow(Xqwl.hWnd);
+
+  // 接收消息
+  while (GetMessage(&msg, NULL, 0, 0)) {
+    TranslateMessage(&msg);
+    DispatchMessage(&msg);
+  }
+  return msg.wParam;
+}
+
+
+
+//
+//  FUNCTION: MyRegisterClass()
+//
+//  PURPOSE: Registers the window class.
+//
+//  COMMENTS:
+//
+//    This function and its usage is only necessary if you want this code
+//    to be compatible with Win32 systems prior to the 'RegisterClassEx'
+//    function that was added to Windows 95. It is important to call this function
+//    so that the application will get 'well formed' small icons associated
+//    with it.
+//
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+	WNDCLASSEX wcex;
+
+	wcex.cbSize = sizeof(WNDCLASSEX); 
+
+	wcex.style			= CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc	= (WNDPROC)WndProc;
+	wcex.cbClsExtra		= 0;
+	wcex.cbWndExtra		= 0;
+	wcex.hInstance		= hInstance;
+	wcex.hIcon			= LoadIcon(hInstance, (LPCTSTR)IDI_BLXQ);
+	wcex.hCursor		= LoadCursor(NULL, IDC_ARROW);
+	wcex.hbrBackground	= (HBRUSH)(COLOR_WINDOW+1);
+	wcex.lpszMenuName	= (LPCSTR)IDC_BLXQ;
+	wcex.lpszClassName	= szWindowClass;
+	wcex.hIconSm		= LoadIcon(wcex.hInstance, (LPCTSTR)IDI_SMALL);
+
+	return RegisterClassEx(&wcex);
+}
+
+//
+//   FUNCTION: InitInstance(HANDLE, int)
+//
+//   PURPOSE: Saves instance handle and creates main window
+//
+//   COMMENTS:
+//
+//        In this function, we save the instance handle in a global variable and
+//        create and display the main program window.
+//
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+   HWND hWnd;
+
+   hInst = hInstance; // Store instance handle in our global variable
+
+   hWnd = CreateWindow(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, NULL, NULL, hInstance, NULL);
+
+   if (!hWnd)
+   {
+      return FALSE;
+   }
+
+   ShowWindow(hWnd, nCmdShow);
+   UpdateWindow(hWnd);
+
+   return TRUE;
+}
+
+//
+//  FUNCTION: WndProc(HWND, unsigned, WORD, LONG)
+//
+//  PURPOSE:  Processes messages for the main window.
+//
+//  WM_COMMAND	- process the application menu
+//  WM_PAINT	- Paint the main window
+//  WM_DESTROY	- post a quit message and return
+//
+//
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	int wmId, wmEvent;
+	PAINTSTRUCT ps;
+	HDC hdc;
+	TCHAR szHello[MAX_LOADSTRING];
+	LoadString(hInst, IDS_HELLO, szHello, MAX_LOADSTRING);
+
+	switch (message) 
+	{
+		case WM_COMMAND:
+			wmId    = LOWORD(wParam); 
+			wmEvent = HIWORD(wParam); 
+			// Parse the menu selections:
+			switch (wmId)
+			{
+				case IDM_ABOUT:
+				   DialogBox(hInst, (LPCTSTR)IDD_ABOUTBOX, hWnd, (DLGPROC)About);
+				   break;
+				case IDM_EXIT:
+				   DestroyWindow(hWnd);
+				   break;
+				default:
+				   return DefWindowProc(hWnd, message, wParam, lParam);
+			}
+			break;
+		case WM_PAINT:
+			hdc = BeginPaint(hWnd, &ps);
+			// TODO: Add any drawing code here...
+			RECT rt;
+			GetClientRect(hWnd, &rt);
+			DrawText(hdc, szHello, strlen(szHello), &rt, DT_CENTER);
+			EndPaint(hWnd, &ps);
+			break;
+		case WM_DESTROY:
+			PostQuitMessage(0);
+			break;
+		default:
+			return DefWindowProc(hWnd, message, wParam, lParam);
+   }
+   return 0;
+}
+
+// Mesage handler for about box.
+LRESULT CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	switch (message)
+	{
+		case WM_INITDIALOG:
+				return TRUE;
+
+		case WM_COMMAND:
+			if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL) 
+			{
+				EndDialog(hDlg, LOWORD(wParam));
+				return TRUE;
+			}
+			break;
+	}
+    return FALSE;
+}
---
+++ files/BLXQ/resource.h
@@ -0,0 +1,44 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by BLXQ.rc
+//
+#define IDC_MYICON                      2
+#define IDD_BLXQ_DIALOG                 102
+#define IDD_ABOUTBOX                    103
+#define IDS_APP_TITLE                   103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDS_HELLO                       106
+#define IDI_BLXQ                        107
+#define IDI_SMALL                       108
+#define IDC_BLXQ                        109
+#define IDM_MAINMENU                    109
+#define IDR_MAINFRAME                   128
+#define IDB_BOARD                       129
+#define IDB_SELECTED                    130
+#define IDB_RK                          131
+#define IDB_RA                          132
+#define IDB_RB                          133
+#define IDB_RN                          134
+#define IDB_RR                          135
+#define IDB_RC                          136
+#define IDB_RP                          137
+#define IDB_BK                          138
+#define IDB_BA                          139
+#define IDB_BB                          140
+#define IDB_BN                          141
+#define IDB_BR                          142
+#define IDB_BC                          143
+#define IDB_BP                          144
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        145
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
